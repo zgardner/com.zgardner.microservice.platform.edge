473dc26abf1eba00f6d039877ecbc7f39e0b8bff
Zach Gardner <zgardner@keyholesoftware.com>
2016-07-04 14:11:26 -0500

    Added Hystrix and zipkin

--- src/main/java/com/zgardner/microservice/platform/edge/Application.java
+++ src/main/java/com/zgardner/microservice/platform/edge/Application.java
@@ -3,11 +3,13 @@ package com.zgardner.microservice.platform.edge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@EnableHystrix
 public class Application 
 {
 	public static void main( String[] args )
